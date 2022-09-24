07208e8de1b0d95679f214e8b1e9c904fbae9576
chandramouli3739 <106798663+chandramouli3739@users.noreply.github.com>
2022-09-24 21:24:30 +0530

    Add files via upload

---
+++ Ineuron.java
@@ -0,0 +1,13 @@
+package assignment;
+
+public class Ineuron {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			System.out.println("INUERON");
+		}
+	}
+
+}
